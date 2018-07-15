9c6dc882ed45a37cbd1e954ed923db052ab238a8
amitverma551 <amitverma551@gmail.com>
2018-07-15 11:28:01 -0700

    slider

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+})()
